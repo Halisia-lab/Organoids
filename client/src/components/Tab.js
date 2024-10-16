f53c7ba4eb4137c434aab0c5733a0dcd44e52839
Halisia <halifa.halisia@gmail.com>
2024-10-16 20:45:18 +0200

    last changes

--- client/src/components/Tab.js
+++ client/src/components/Tab.js
@@ -1,6 +1,6 @@
 function Tab({ name, activeTab, changeTab }) {
     return (
-        <button className={`${activeTab === name ? "text-sky-300" : ""} bg-black bg-opacity-30 text-2xl py-4 text-center hover:cursor-pointer hover:bg-sky-500 hover:text-black`} onClick={() => changeTab(name)}>
+        <button className={`${activeTab === name ? "text-sky-300" : ""} bg-black bg-opacity-30 text-2xl py-4 text-center hover:cursor-pointer hover:bg-sky-500 hover:text-black hover:animate-zoomin`} onClick={() => changeTab(name)}>
             {name}
         </button>
     )
